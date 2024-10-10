f0e7ddc8aa6d33ca69063b7412bed9a374f63c99
Aswani25 <kumar.aswanig@gmail.com>
2024-10-10 23:20:42 +0530

    added validation of dropdown list

--- tests/dropdown.spec.ts
+++ tests/dropdown.spec.ts
@@ -12,7 +12,25 @@ test('Select values from dropdown', async({page}) => {
     await page.locator('#state').selectOption({index: 3});
     await page.waitForTimeout(3000);
 
-    const dropdownValues = await page.locator('#state').textContent();
-    expect(dropdownValues?.includes('Bihar')).toBeTruthy()
+    const dropdownValues: any = await page.locator('#state').textContent();
+    expect(dropdownValues.includes('Bihar')).toBeTruthy()
     expect(dropdownValues).toContain('Goa');
+});
+
+test.only('Select values from dropdown 1', async({page}) => {
+    await page.goto('https://freelance-learn-automation.vercel.app/signup');
+    let state: any = await page.$('#state');
+    await page.locator('#state').selectOption({label: 'Goa'});
+    let allElements = await page.$$('option');
+    let ddStatus = false;
+    for(let i=0; i < allElements.length; i++){
+        let element = allElements[i];
+        let value: any = await element.textContent();
+        console.log(value);
+        if(value === "Mizoram"){
+            ddStatus = true;
+            break;
+        }        
+    }
+    expect(ddStatus).toBeTruthy();
 });
